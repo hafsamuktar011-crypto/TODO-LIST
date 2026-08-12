834cd1cb359c6eeb3e1b1c43d63610ceb35435bf
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-12 12:17:29 +0300

    build: install react-icons dependency

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
 
 function ToDoForm() {
   return (
     <section>
-      <input type="text" id="todolist_input" placeholder='Search note...' />
+        <div>
+          <input type="text" id="todolist_input" placeholder='Search note...' />
+          <CiSearch />
+        </div>
+      
     </section>
   )
 }
