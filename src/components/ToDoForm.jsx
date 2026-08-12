10121af46b6affabc0513398d3b798db3bce1872
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-12 12:10:40 +0300

    feat:add todo input field

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ToDoForm() {
   return (
     <section>
-      
+      <input type="text" id="todolist_input" placeholder='Search note...' />
     </section>
   )
 }
