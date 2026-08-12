1c223bc408ff326fdfb8c4d51b8deb5c6e4b9d28
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-12 16:34:01 +0300

    style:style todo list

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { FaPlusCircle } from "react-icons/fa";
+import { FcNeutralDecision } from "react-icons/fc";
 function ToDoList() {
+
   return (
-    <section>
-        <img src="/images/empty-todo.png" alt="" />
-      <FaPlusCircle />
+    <section className='relative flex flex-col items-center mt-10'>
+        <img src="/images/empty-todo.png" alt="" 
+        className='flex justify-center items-center '/>
+        <div className='flex items-center px-1 m-2 '>
+         <p>it is empty lazy boy!!</p>
+        <FcNeutralDecision />
+        </div>
+        
+      <FaPlusCircle className='absolute top-100 right-9 -translate-y-1/6' />
     </section>
   )
 }
