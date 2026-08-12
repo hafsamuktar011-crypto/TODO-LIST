644281356efde1918c75fd3412d540d8a8aa3bc5
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-12 12:24:44 +0300

    feat:add todo select and mode icon

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
-
+import { MdOutlineWbSunny } from "react-icons/md";
 function ToDoForm() {
   return (
     <section>
         <div>
           <input type="text" id="todolist_input" placeholder='Search note...' />
           <CiSearch />
+          <select>
+            <option value="">All</option>
+            <option value="">Not Done</option>
+            <option value="">Done</option>
+          </select>
+          <MdOutlineWbSunny />
         </div>
       
     </section>
