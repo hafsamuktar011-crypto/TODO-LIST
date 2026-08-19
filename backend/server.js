c7cc3f3b535d7f22a9d11f66986bd1841ea6dd32
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-19 17:36:05 +0300

    feat: add MySQL database connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
+import express from "express"
+import mysql from "mysql2"
+
+
 const server=express()
 
+//create connection
+const connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    port:3306
+   })
+connection.connect((err)=>{
+    if(err){
+        console.log("MySQL connection failed:",err.message)
+        return
+    }
+})
 
 
 server.listen(1000,(err)=>{
